c58c6edd13165ddb8d49cda85612d93d8524f32e
Anton Felich <anton.felich@gmail.com>
2018-02-07 09:48:40 +1100

    PageUp code challenge

---
+++ Calculator.fs
@@ -0,0 +1,27 @@
+﻿module Calculator
+
+type Parcel = {
+    weight: float
+    width: int
+    height: int
+    depth: int
+}
+
+type CalculatorResult = Result<decimal, string>
+type Rule = Parcel -> CalculatorResult option
+
+let calculate (rules: Rule seq) (parcel: Parcel): string =
+
+    let picker = 
+        (fun rule -> 
+            parcel 
+            |> rule
+        )   
+    
+    match rules |> Seq.tryPick picker with
+    | Some (Ok value) ->
+        value |> string
+    | Some (Error error) ->
+        error |> sprintf "Error: %s"
+    | None ->
+        "no matching rules"
